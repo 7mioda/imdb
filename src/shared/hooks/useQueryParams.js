fe05c2d542962b19cd0a7a8690a96cad7bf00ea2
Edaly Ahmed <ahmed.edaly@oyez.fr>
2019-11-25 23:05:15 +0100

    (feat) optimizing useQueryParmas hook

--- src/shared/hooks/useQueryParams.js
+++ src/shared/hooks/useQueryParams.js
@@ -1,10 +1,8 @@
 import { useLocation } from 'react-router-dom';
-import { parse } from 'querystring';
 
 
 const useQueryParams = () => {
-  const { search } = useLocation();
-  return parse(search.slice(1));
+  return new URLSearchParams(useLocation().search);
 };
 
 
