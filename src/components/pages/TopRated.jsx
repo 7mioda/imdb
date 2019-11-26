2d894905560e5147d7ba6346de0c5356bd60f4be
Edaly Ahmed <ahmed.edaly@oyez.fr>
2019-11-26 17:18:20 +0100

    (feat) adding pagination to top-rated page

--- src/components/pages/TopRated.jsx
+++ src/components/pages/TopRated.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Page from '../organisms/Page/Page';
 import useFetcher from '../../shared/hooks/useFetcher';
+import Paginator from '../Paginator/Paginator';
+import useQueryParams from "../../shared/hooks/useQueryParams";
 
-export default () => {
-  const { data, loading, error } = useFetcher({ url: '/movie/top_rated', queryString: { page: 1 } });
+export default (props) => {
+  const query = useQueryParams(props);
+  const { data, loading, error } = useFetcher({ url: '/movie/top_rated', queryString: { page: query.get('page') } });
   console.log(data, loading, error);
   return (
     <Page title="IMDb Rop 250 - IMDb">
             Movie
+      <Paginator count={1000} nbrPerPage={20} />
     </Page>
   );
 };
--- src/shared/hooks/useFetcher.js
+++ src/shared/hooks/useFetcher.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import NProgress from 'nprogress';
 import axios from 'axios';
 import { stringify } from 'querystring';
