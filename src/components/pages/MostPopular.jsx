87860c9dc74be43d1833599c0f3ae2442165c985
Edaly Ahmed <ahmed.edaly@oyez.fr>
2019-11-26 01:00:33 +0100

    (feat) adding paginator hook

--- src/components/pages/MostPopular.jsx
+++ src/components/pages/MostPopular.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { get } from 'lodash';
 import Page from '../organisms/Page/Page';
 import useFetcher from '../../shared/hooks/useFetcher';
+import usePaginator from '../../shared/hooks/usePaginator';
 import useQueryParams from '../../shared/hooks/useQueryParams';
 
 const MostPopular = (props) => {
-  const { page = 1 } = useQueryParams();
-  const { data, loading, error } = useFetcher({ url: '/movie/popular', queryString: { page } });
+  const query = useQueryParams(props);
+  console.log(query.get('page'));
+  const { data, loading, error } = useFetcher({ url: '/movie/popular', queryString: { page: query.get('page') } });
+  const { pageCount, setNextPage } = usePaginator(get(data, 'total_results', 1000), 20);
+
   console.log(data, loading, error);
   return (
     <Page {...props} title="Most Popular Movies - IMDb">
             Showtimes and Cinemas tickets
+      <button type="button" onClick={() => setNextPage(5)}>next</button>
     </Page>
   );
 };
