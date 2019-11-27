f42bff0e922edaf6ea1fbde89671581bc8e95b10
Edaly Ahmed <ahmed.edaly@oyez.fr>
2019-11-27 11:51:50 +0100

    (feat) adding search result dropdown to compoenent

--- src/components/molecules/SreachInput/SearchInput.jsx
+++ src/components/molecules/SreachInput/SearchInput.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Input from '../../atoms/Input/Input';
 import SearchIcon from '../../atoms/SearchIcon/SearchIcon';
+import withStyle from './withStyle';
+import SearchResultItem from '../SearchResult/SearchResultItem';
+import useOnClickOutside from '../../../shared/hooks/useOnClickOutside';
 
 
-const SearchInput = () => (
-  <Input highlited height="30px" placeholder="Search IMDb" Suffix={(<SearchIcon />)} />
-);
+const SearchInput = ({ className, suggestion = true }) => {
+  const ref = useRef();
+  const [showSuggestion, setShowSuggestion] = useState(false);
+  useOnClickOutside(ref, () => setShowSuggestion(false));
+  return (
+    <div ref={ref} className={`${className}`}>
+      <Input highlighted height="30px" onFocus={() => setShowSuggestion(true)} placeholder="    Search IMDb" Suffix={(<SearchIcon />)} />
+      {suggestion && showSuggestion && (
+        <div className="search__suggestion">
+          <SearchResultItem data={{ title: 'title', release_date: 'releaseDate', backdrop_path: 'https://m.media-amazon.com/images/M/MV5BYzljYzY1ZjItYzNkMC00Mjc2LWFiOGYtMmFkNGViOTE1ZmMwXkEyXkFqcGdeQXVyNDYzNjU3ODM@._V1_UY74_CR12,0,50,74_.jpg' }} />
+          <SearchResultItem data={{ title: 'title', release_date: 'releaseDate', backdrop_path: 'https://m.media-amazon.com/images/M/MV5BYzljYzY1ZjItYzNkMC00Mjc2LWFiOGYtMmFkNGViOTE1ZmMwXkEyXkFqcGdeQXVyNDYzNjU3ODM@._V1_UY74_CR12,0,50,74_.jpg' }} />
+          <SearchResultItem data={{ title: 'title', release_date: 'releaseDate', backdrop_path: 'https://m.media-amazon.com/images/M/MV5BYzljYzY1ZjItYzNkMC00Mjc2LWFiOGYtMmFkNGViOTE1ZmMwXkEyXkFqcGdeQXVyNDYzNjU3ODM@._V1_UY74_CR12,0,50,74_.jpg' }} />
+
+        </div>
+      )}
+    </div>
+  );
+};
 
 
-export default SearchInput;
+export default withStyle(SearchInput);
