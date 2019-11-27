4039050ccd8da5a20b822b940871f0b2db67f784
Edaly Ahmed <ahmed.edaly@oyez.fr>
2019-11-27 19:06:58 +0100

    (feat)  updating pages content

--- src/components/pages/MostPopular.jsx
+++ src/components/pages/MostPopular.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { get } from 'lodash';
 import Page from '../organisms/Page/Page';
 import useFetcher from '../../shared/hooks/useFetcher';
 import useQueryParams from '../../shared/hooks/useQueryParams';
-import Paginator from '../Paginator/Paginator';
+import Paginator from '../molecules/Paginator/Paginator';
+import Card from '../molecules/Card/Card';
 
 const MostPopular = (props) => {
   const query = useQueryParams(props);
-  const { data, loading, error } = useFetcher({ url: '/movie/popular', queryString: { page: query.get('page') } });
+  const page = query.get('page');
+  const { data, loading, error } = useFetcher({ url: '/movie/popular', queryString: { page } });
   console.log('fetcg', data, loading, error);
   return (
     <Page {...props} title="Most Popular Movies - IMDb">
-            Showtimes and Cinemas tickets { data && data.page}
-      <Paginator count={1000} nbrPerPage={20} />
+      <div
+        style={{
+          display: 'flex', flexWrap: 'wrap', width: '100%', justifyContent: 'center', maxWidth: 1470,
+        }}
+      >
+        {get(data, 'results', []).map((movie, index) => <Card key={index.toString()} movie={movie} />)}
+      </div>
+      <div
+        style={{
+          display: 'flex', flexWrap: 'wrap', width: '100%', justifyContent: 'center', margin: '25px',
+        }}
+      >
+        <Paginator count={1000} nbrPerPage={20} activePage={page} />
+      </div>
     </Page>
   );
 };
--- src/components/pages/TopRated.jsx
+++ src/components/pages/TopRated.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { get } from 'lodash';
 import Page from '../organisms/Page/Page';
 import useFetcher from '../../shared/hooks/useFetcher';
-import Paginator from '../Paginator/Paginator';
+import Paginator from '../molecules/Paginator/Paginator';
 import useQueryParams from '../../shared/hooks/useQueryParams';
+import Card from '../molecules/Card/Card';
 
 export default (props) => {
   const query = useQueryParams(props);
-  const { data, loading, error } = useFetcher({ url: '/movie/top_rated', queryString: { page: query.get('page') } });
-  console.log(data, loading, error);
+  const page = query.get('page');
+  const { data, loading, error } = useFetcher({ url: '/movie/top_rated', queryString: { page } });
+  console.log(data);
   return (
     <Page title="IMDb Rop 250 - IMDb">
-            Movie
-      <Paginator count={1000} nbrPerPage={20} />
+      <div
+        style={{
+          display: 'flex', flexWrap: 'wrap', width: '100%', justifyContent: 'center', maxWidth: 1470,
+        }}
+      >
+        {get(data, 'results', []).map((movie, index) => <Card key={index.toString()} movie={movie} />)}
+      </div>
+      <div
+        style={{
+          display: 'flex', flexWrap: 'wrap', width: '100%', justifyContent: 'center', margin: '25px',
+        }}
+      >
+        <Paginator count={1000} nbrPerPage={20} activePage={page} />
+      </div>
     </Page>
   );
 };
