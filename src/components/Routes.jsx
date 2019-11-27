10315740bedf3c64afd6da26be3e2a8343c4b851
Edaly Ahmed <ahmed.edaly@oyez.fr>
2019-11-27 19:05:37 +0100

    (fix)  removing csstransition

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import {
-  TransitionGroup,
-  CSSTransition,
-} from 'react-transition-group';
-import {
-  Switch, Route, useLocation,
+  Switch, Route,
 } from 'react-router-dom';
 import Home from './pages/Home';
 import MovieDetails from './pages/MovieDetails';
@@ -14,23 +10,14 @@ import MostPopular from './pages/MostPopular';
 import NotFound from './pages/NotFound';
 
 export default () => {
-  const location = useLocation();
   return (
-    <TransitionGroup>
-      <CSSTransition
-        key={location.key}
-        classNames="fade"
-        timeout={300}
-      >
-        <Switch>
-          <Route path="/filmify" exact component={Home} />
-          <Route path="/filmify/movie-details/:id" component={MovieDetails} />
-          <Route path="/filmify/top" component={TopRated} />
-          <Route path="/filmify/showtimes" component={Showtimes} />
-          <Route path="/filmify/popular" component={MostPopular} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </CSSTransition>
-    </TransitionGroup>
+    <Switch>
+      <Route path="/filmify" exact component={Home} />
+      <Route path="/filmify/movie-details/:id" component={MovieDetails} />
+      <Route path="/filmify/top" component={TopRated} />
+      <Route path="/filmify/showtimes" component={Showtimes} />
+      <Route path="/filmify/popular" component={MostPopular} />
+      <Route path="*" component={NotFound} />
+    </Switch>
   );
 };
