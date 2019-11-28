d107ef8aa77959fa1d362ca691a220bb032704c6
Edaly Ahmed <ahmed.edaly@oyez.fr>
2019-11-28 19:08:00 +0100

    (fix) fixing input styles

--- src/components/atoms/Input/withStyle.js
+++ src/components/atoms/Input/withStyle.js
@@ -4,46 +4,31 @@ export default (component) => styled(component)`
   position: relative;
   width: ${({ width }) => width || '100%'};
   box-sizing: border-box;
+  border: 1px solid rgba(128, 128, 128, 0.32);
+  display: flex;
+  background-color: ${({ theme: { colors: { primary } } }) => primary};
+  
   
   input {
-    border: 1px solid rgba(128, 128, 128, 0.32);
-    width: 100%;
+    padding: 0 1rem;
+    border-style: none;
     height: ${({ height }) => height || '100%'};
-    border-radius: 5px;
     color: ${({ theme: { colors: { tertiary } } }) => tertiary};
-    background-color: ${({ theme: { colors: { primary } } }) => primary};
     font-family: inherit;
-    &:focus {
-      background-color: ${({ theme: { colors: { secondary } } }) => secondary};
-    }
-  }
-  .highlighted {
-      &:focus {
-          border-color: #fff;
-      }
-  }
-  .highlighted:focus {
-    border: 1px solid rgba(128, 128, 128, 0.9);
+    background-color: inherit;
+    flex-grow: 1;
+    outline: none;
   }
   .rounded {
     border-radius: 40px;
     padding: ${({ padding }) => padding || '0 30px'};
   }
   .prefix {
-    position: absolute;
-    height: 100%;
     top: 50%;
-    transform: translateY(-50%);
-    left: 0;
   }
   .suffix {
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100%;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    right: 0;
   }
 `;
