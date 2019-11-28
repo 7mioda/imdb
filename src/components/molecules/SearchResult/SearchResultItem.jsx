8584e535eab6ce1e05f864f4e8d53b59752f18e9
Edaly Ahmed <ahmed.edaly@oyez.fr>
2019-11-28 19:09:36 +0100

    (feat) handling different image paths

--- src/components/molecules/SearchResult/SearchResultItem.jsx
+++ src/components/molecules/SearchResult/SearchResultItem.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import { isEmpty, compact, isUndefined } from 'lodash';
 import withStyle from './withStyle';
 
 
+const getImg = (base, ...paths) => {
+  if (isEmpty(compact(paths))) return undefined;
+  return (`${base}${paths.find((e) => !isUndefined(e))}`);
+};
+
 const SearchResultItem = ({
-  className, data: { title, release_date: releaseDate, backdrop_path: backdropPath, poster_path },
+  className, data: {
+    title,
+    name,
+    release_date: releaseDate,
+    poster_path: poster,
+    logo_path: logo,
+    profile_path: profile,
+  },
 }) => (
   <div className={`${className}`}>
     <div className="search-item__img">
-      <img style={{ width: 50, height: 70 }} src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt="" />
+      <img style={{ width: 50, height: 70 }} src={getImg('https://image.tmdb.org/t/p/w500/', poster, profile, logo) || '/default-search.jpeg'} alt="" />
     </div>
     <div className="search-item__content">
-      <h3>{title}</h3>
+      <h3>{title || name}</h3>
       <h4>{releaseDate && new Date(releaseDate).getFullYear()}</h4>
     </div>
   </div>
