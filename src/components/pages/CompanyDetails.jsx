c6af3c41f327dea87e6ebfe6ef8de43d0e1cf6e0
Edaly Ahmed <ahmed.edaly@oyez.fr>
2019-12-01 19:25:31 +0100

    (fix) fixing details page titles

--- src/components/pages/CompanyDetails.jsx
+++ src/components/pages/CompanyDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { get } from 'lodash';
 import Page from '../organisms/Page/Page';
 import useFetcher from '../../shared/hooks/useFetcher';
 
@@ -8,8 +9,9 @@ export default () => {
   console.log(companyId);
   const { data } = useFetcher({ url: `/company/${companyId}` });
   console.log(data);
+  const name = get(data, 'name', 'Celebrity');
   return (
-    <Page>
+    <Page title={`${name} - IMDb`}>
             Company
     </Page>
   );
